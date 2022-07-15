c5cce050804bbfba48bf93bb29b7e85b90dd2ac1
AJ Brown <ansleybrown1337@gmail.com>
2022-07-15 14:48:21 -0600

    Update file-merger.R

--- Code/file-merger.R
+++ Code/file-merger.R
@@ -1,13 +1,17 @@
 # Steamboat Data Clean and Merge 2022
-# AJ Brown
+# AJ Brown and Caz Bell
 # 7/13/2022
 
-# Tool to merge multiple xcel files into one and create categories for analysis
+# TODO create fxns: 1) processing df, 2) clean data for graphing, 3) graphing
+
+# Tool to merge multiple excel files into one and create categories for analysis
 
 library(magrittr)
 library(dplyr)
 library(readr)
 library(readxl)
+library(ggplot2)
+library(lattice)
 
 #Change directory to where ever needed, make sure to replace "\" with "/"
 directory = 'C:/Users/ansleybr/OneDrive - Colostate/AWQP/Caz Steamboat Data'
@@ -19,12 +23,10 @@ df <- list.files(path=directory) %>%
   lapply(read_xls) %>% 
   bind_rows
 
-df$IndTestName = gsub(" ", "_", df$IndTestName)
-
 # Create Inflow/outflow/other source column based on FieldID
 df$source = ifelse(grepl('IN', df$FieldID), 'Inflow', 
                    ifelse(grepl('OT|UYM', df$FieldID), 'Outflow',
-                           ifelse(grepl('LABQC', df$FieldID),'LABQC', 'Other')))
+                           ifelse(grepl('LABQC', df$FieldID),'LABQC', 'Point_Sample')))
 
 # Create location column based on FieldID
 df$location = ifelse(grepl('SCI',df$FieldID), 'Stagecoach_In',
@@ -37,15 +39,39 @@ df$location = ifelse(grepl('SCI',df$FieldID), 'Stagecoach_In',
 # Create sample type column base on FieldID
 df$type = ifelse(grepl('G|GB|DB',df$FieldID), 'Grab',
                      ifelse(grepl('LC', df$FieldID),'Low_Cost', 
-                            ifelse(grepl('ISC!LC!GB', df$FieldID), 'ISCO', NA)))
-
+                            ifelse(grepl('LABQC', df$FieldID), 'LABQC', 'ISCO')))
+# Create subset dataframe to check categorization algorithm
 dfID = df[c('FieldID','type')]
 
-# Important Graphs
-# Goal 1: Get rid of unnecessary columns
-# Goal 2: Make important graphs for each location illustrating inflow v. outflow
-  # Nitrate, Total P, Ortho-P on same graph for example, selenium on separate due to units
-        # Pro-tip: if ggplot is sucking b/c your df is too big, then consider dropping unneeded rows to simplify
-             # e.g., drop all rows with locations other than the ones you're using
 
+# DATA CLEANING
+# Drop LABQC columns
+df = subset(df, df$type != 'LABQC')
+
+# drop columns containing only NAs
+df = Filter(function(x)!all(is.na(x)), df)
+
+# replace spaces in test name column for graphing
+df$IndTestName = gsub(" ", "_", df$IndTestName)
+
+# convert results to numeric values
+df$FinalResult = as.numeric(df$FinalResult)
+
+
+# Important Graphs
+
+# Generic bar plot tool
+
+df_a = subset(df, df$IndTestName == 'TOTAL_PHOSPHORUS')
+#df_P = subset(df_a, df_a$location == 'Legacy_Ranch' | df_a$location == 'Upper_Yampa')
+
+p = ggplot(df_P, aes(x = source, y = FinalResult, fill=source)) +
+           geom_bar(stat='identity') +
+           facet_wrap(~location) +
+           xlab('') +
+           ylab('Total Phosphorus, mg/L') +
+           ggtitle('Water Quality Analysis for Yampa River Valley') +
+           theme_bw() +
+           theme(axis.title.y = element_text(margin=margin(t = 0, r = 20, b = 0, l = 0)))
+p
 
