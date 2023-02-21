c3e5debbc5c45386510cb44f850dd1ce0b094168
ansleybrown1337 <ansleybrown1337@gmail.com>
2023-02-21 14:55:29 -0700

    first functional version
    ready for field testing

--- Code/file-merger.R
+++ Code/file-merger.R
@@ -1,24 +1,27 @@
 # ALS to AWQP Data Clean and Merge Tool
-# AJ Brown and Caz Bell
-# Started: 7/13/2022
-# First working version: 20 Feb 2023
+# AJ Brown, ansley.brown@colostate.edu
+# Started: 13 July 2022
+# Updated: 21 Feb 2023
 
-# Tool to clean and merge multiple htm files directly downloaded
-# from the ALS global portal into one and create categories for
-# future analysis by AWQP staff.
+# Tool to clean and merge multiple .htm files directly downloaded
+# from the ALS global portal into one master file and create categories for
+# further analysis.
 
-# Script process flow:
-# 1) Define global variables
- # 1a) Working file_path
- # 1b) Set the default file directory
- # 1c) Define dictionaries for interpreting sample ID codes
-# 2) Import data
-# 3) Clean data
-# 4) Process data
- # this will involve the creation of dictionaries to convert codes to names
- # it will also create new identifying columns based on the dictionaries
-# 5) Repeat for each htm file desired and merge into one df
-# 6) Export data as csv
+# Script work flow:
+  # 0) Import libraries
+  # 1) Define global variables
+  # 1a) Working file_path
+  # 1b) Set the default file directory
+  # 1c) Define dictionaries for interpreting sample ID codes
+  # 2) Define functions
+  # 2a) map_values <-- a function to map sample ID text to dictionary values
+  # 2b) import data
+  # 2c) clean data
+  # 2d) process data
+  # 2e) execute previous functions
+  # 2f) repeat for all files in a directory
+  # 4) View resulting dataframe(s) for QA/QC
+  # 3) Export data as csv
 
 # Import libraries
 package.list <- c("magrittr",
@@ -47,7 +50,7 @@ file_path <- "./Example Data/AVRC.xls"
  # Set the default file directory to the directory containing the selected file
 directory <- dirname(file_path)
  # Dictionaries for interpreting sample ID codes
-
+  # Add to these at needed for new locations, treatments, methods, etc.
 location.dict <- c(
   "Berthoud" = "BT",
   "ARDEC South - Org" = "ASO",
@@ -104,7 +107,7 @@ eventType.dict <- c(
   "Outflow" = c("OUT", "OT")
   )
 
-# Functions
+# Define Functions
 map_values <- function(text, dict) {
  # function to map sample ID text to dictionary values
   # Split the text by spaces
@@ -150,7 +153,7 @@ cleanData <- function(df) {
     # convert values containing "<" to 0
     mutate(RESULT = ifelse(grepl("<", RESULT), 0, RESULT),
          # create column to indicate if a result value was a non-detect
-         non_detect = ifelse(RESULT == 0, TRUE, FALSE),
+         non.detect = ifelse(RESULT == 0, TRUE, FALSE),
          # change "N/A" to NA in any column
          across(everything(), ~ ifelse(. == "N/A", NA, .))) %>%
     # convert select columns to numeric if needed
@@ -166,10 +169,12 @@ cleanData <- function(df) {
 
 processData <- function(df) {
   # Process data to create new columns for analysis based on ID codes
+    # create a list of columns for post-processing
   text_cols <- c("location.name",
                    "treatment.name",
                    "method.name",
                    "event.type")
+    # create new columns based on ID codes
   df %>%
     mutate(
       # create duplicate column
@@ -184,11 +189,16 @@ processData <- function(df) {
       # create event type name column based on Sample ID
       event.type = sapply(SAMPLE.ID, function(x) map_values(x, eventType.dict))
       ) %>%
-    # if event type is NA, use "Point Sample" as default
-    mutate(event.type = if_else(is.na(event.type), "Point Sample", event.type)
-    ) %>%
     # remove numbers from new columns due to dict mapping
-    mutate_at(text_cols, ~ gsub("[0-9]", "", .))
+     # caution: if there are more than 10 dict keys, this will not work
+     # note: avoid naming future locations with numbers in the name
+    mutate_at(c("location.name",
+                "method.name",
+                "event.type"), ~ gsub("[0-9]", "", .)) %>%
+     # treatment.name needs special treament because of CT/MT/ST 1/2 having #'s
+    mutate_at("treatment.name", ~ substr(., 1, nchar(.) - 1)) %>%
+    # if event.type is NA, use "Point Sample" as default
+    mutate(event.type = if_else(is.na(event.type), "Point Sample", event.type))
 }
 
 executeFxns <- function(file_path) {
@@ -208,15 +218,13 @@ listFiles <- function(directory) {
   return(df)
 }
 
-# Execute functions and view resulting dataframe:
+# View resulting dataframe(s) for QA/QC
  # For a single file
 df_final <- executeFxns(file_path)
 View(df_final)
- # For multiple files in a given directory
+ # For all .xls (but actually .htm) files in a given directory
 df_final_merged <- listFiles(directory)
 View(df_final_merged)
 
 # Export the combined data frame as a CSV file
 write.csv(df_final_merged, file = "combined_data.csv", row.names = FALSE)
- # Print a confirmation message
-cat("Combined data exported as 'combined_data.csv'.\n")
