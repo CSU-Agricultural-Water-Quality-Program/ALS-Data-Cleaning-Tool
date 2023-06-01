03e3bc906e4378d60310c96f710b4cdfa11a70b5
mollycb8 <mollycbell8@gmail.com>
2023-06-01 11:34:48 -0600

    update
    weekly_report is running for time set but not a weekly report

--- Code/weekly_report.R
+++ Code/weekly_report.R
@@ -10,32 +10,39 @@ library(knitr)
 rmd_file <- "water_report.Rmd"
 r_script <- knitr::purl(rmd_file)
 source(r_script)
-#create function for todays date
-today <- function() {
-  return(Sys.Date())
-}
-# render rmd file and output into report folder
-rmarkdown::render(input = 'water_report.Rmd', output_file = sprintf('~/GitHub/ALS-Data-Cleaning-Tool/Report/water_report_%s.html', 
-                                                                    format(today, format = "%m-%d-%y")))
-library(later)
+
+library(rmarkdown)
+
+# Generate the output file name with today's date
+output_file <- sprintf('~/GitHub/ALS-Data-Cleaning-Tool/Report/water_report_%s.html',
+                       format(today(), format = "%m-%d-%y"))
+
+# Render the R Markdown document
+rmarkdown::render(input = 'water_report.Rmd', output_file = output_file)
+
+
 # Set the path to the R script
+
 script_path <- "weekly_report.R"
 
 # Define the function to execute the R script
 
-execute_script <- function() {
-  source(script_path)
-}
+execute_script <- function(){
+  source(script_path)}
 
 
 
-execution_time <- as.POSIXct(paste(today(), "10:45:00"))
+execution_time <- as.POSIXct(paste(today(), "11:32:00"))
 
-# Calculate the delay in seconds
+
 delay_seconds <- as.numeric(execution_time - Sys.time())
 
-# Schedule the execution of the R script
+
 later::later(execute_script, delay = delay_seconds)
+
+
+
+
 
 
 
