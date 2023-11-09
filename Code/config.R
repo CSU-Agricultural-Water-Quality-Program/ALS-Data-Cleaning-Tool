a71a42ee9a0927b541dbedee4d010948fda70fd4
ansleybrown1337 <ansleybrown1337@gmail.com>
2023-11-08 20:06:29 -0700

    added config file

---
+++ Code/config.R
@@ -0,0 +1,22 @@
+# configuration file for ALS data cleaning tool
+# AJ Brown
+# Ansley.Brown@colostate.edu
+# 3 Nov 2023
+
+# How to:
+# Define your file paths below to be used in this tool.  Please note that these
+# variables are found in the file-merger.r script, which ultimately controls the
+# file path for all other scripts.
+
+# Definitions:
+# directory = where ALS raw data is stored (xls and htm)
+# tss_file_path = where TSS data excel file is stored (xlsx)
+
+# For GitHub  Repo
+  directory <- "./Data"
+  tss_file_path <- './TSS/TSS_Master_2023.xlsx'
+# for real data in file that won't be uploaded to github repo:
+  # directory <- "./Confidential Data"
+  # tss_file_path <- './Confidential TSS/TSS_Master_2023.xlsx'
+  
+tss_directory<- dirname(tss_file_path)
