319d7b2bec7c3ac8ab8959f0f175c1bf2ec7c331
ansleybrown1337 <ansleybrown1337@gmail.com>
2023-11-08 20:15:44 -0700

    updated config

--- Code/config.R
+++ Code/config.R
@@ -13,10 +13,13 @@
 # tss_file_path = where TSS data excel file is stored (xlsx)
 
 # For GitHub  Repo
-  directory <- "./Data"
-  tss_file_path <- './TSS/TSS_Master_2023.xlsx'
+  # directory <- "./Data"
+  # tss_file_path <- './TSS/TSS_Master_2023.xlsx'
 # for real data in file that won't be uploaded to github repo:
-  # directory <- "./Confidential Data"
-  # tss_file_path <- './Confidential TSS/TSS_Master_2023.xlsx'
-  
+  # directory <- "{your file path to ALS data FOLDER here}"
+  # tss_file_path <- '{your file path to TSS data FILE here}'
+# Example
+  directory <- "./Confidential Data"
+  tss_file_path <- './Confidential TSS/TSS_Master_2023.xlsx'
+
 tss_directory<- dirname(tss_file_path)
