893a672933bd3fc2e77b10053d97dba5b94c3490
ansleybrown1337 <ansleybrown1337@gmail.com>
2024-05-07 09:52:10 -0600

    ran for berthoud

--- Code/config.R
+++ Code/config.R
@@ -23,16 +23,16 @@ create_directory_if_not_exists <- function(directory_path) {
 }
 
 # Native GitHub Repo Folders:
-# directory <- "./Data"
-# tss_file_path <- './TSS/TSS_Master_2023.xlsx'
+directory <- "./Data"
+tss_file_path <- './TSS/TSS_Master_2023.xlsx'
 
 # for real data in file that won't be uploaded to github repo:
 # directory <- "{your file path to ALS data FOLDER here}"
 # tss_file_path <- '{your file path to TSS data FILE here}'
 
 # Example using real data files for and create folder function:
-directory <- "./Confidential Data"
-tss_file_path <- './Confidential TSS/TSS_Master_2023.xlsx'
+#directory <- "./Confidential Data"
+#tss_file_path <- './Confidential TSS/TSS_Master_2023.xlsx'
 
 create_directory_if_not_exists(directory)
 create_directory_if_not_exists(dirname(tss_file_path))
